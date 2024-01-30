1fccb0d0c0b5f88019d8546ce49ce7b80b365daa
pranotipatil23 <patilpranu5@gmail.com>
2024-01-30 19:57:43 +0530

    Fixed CSS on body restaurant cards

--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -1,23 +1,42 @@
 import { CDN_URL } from "../utils/constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 const RestaurantCard = (props) => {
   const { restaurants } = props;
 
   const { info } = restaurants;
 
-  return (
-    <div className="res-card m-4 p-4 w-[200px] rounded-lg bg-gray-100 hover:bg-gray-200">
-      <img
-        className="res-logo rounded-lg"
-        src={CDN_URL + info?.cloudinaryImageId}
-        alt="res logo"
-      />
+  console.log(info);
 
-      <h3 className="font-bold py-4 text-lg">{info.name}</h3>
-      <h4 className="res-cuisine">{info.cuisines.join(", ")}</h4>
-      <h4 className="py-4 ">{info?.avgRating}</h4>
-      <h4 className="py-4">{info?.costForTwo}</h4>
-      <h4>{info?.sla?.deliveryTime}</h4>
+  return (
+    <div className="res-card m-4 p-4 w-[324px] h-[375px] max-h-[400px] max-w-[350px] rounded-lg hover:bg-gray-50">
+      <div className="mt-4">
+        <img
+          className="res-logo rounded-lg w-full h-48 object-cover"
+          src={CDN_URL + info?.cloudinaryImageId}
+          alt="res logo"
+        />
+      </div>
+      <div>
+        <div className="font-sans py-2 text-xl">{info.name}</div>
+        <div className="py-2">
+          <div>
+            <span className="text-green-600 mx-1">
+              <FontAwesomeIcon icon={faStar} />
+            </span>
+            <span>{info?.avgRating}</span>
+            <span className="text-2xl font-bold items-center justify-center">
+              {" . "}
+            </span>
+            <span>{info?.sla?.slaString}</span>
+          </div>
+        </div>
+        <h4 className="res-cuisine font-thin whitespace-nowrap overflow-ellipsis overflow-hidden">
+          {info.cuisines.join(", ")}
+        </h4>
+        <span className="font-thin font-sans">{info?.locality}</span>
+      </div>
     </div>
   );
 };
